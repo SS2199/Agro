34e9a78577d3a8a65f0ae2f42732ab4a6883e4fc
sindhusambasivam <95736922+sindhusambasivam@users.noreply.github.com>
2023-04-05 11:05:57 +0000

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CarosalComponent } from './carosal/carosal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    CarosalComponent
   ],
   imports: [
     BrowserModule,
+    CarouselModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
